4191732c262fc51020f2843825b6e4792db1c6a9
bookhabit <qnrgoqlt@naver.com>
2024-05-08 00:40:38 +0900

    프로젝트세팅 및 배경이미지 , 검색바 컴포넌트

---
+++ app/components/SearchBar/SearchBarForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBarForm = () => {
+    return (
+        <div className='flex justify-center items-center w-full max-w-lg relative'>
+            <input 
+                type="text" 
+                placeholder='Search for a team'
+                className='w-full bg-gradient-to-r from-neutral-100/60 to-black/25 bg-transparent p-2 outline-none border-neutral-100/60 border-[1px] rounded-xl hover:border-blue-400 focus:border-blue-400 focus:from-blue-400/60 text-neutral-100 placeholder:text-neutral-100/70'
+            />
+        </div>
+    );
+};
+
+export default SearchBarForm;
